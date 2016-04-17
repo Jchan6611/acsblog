e5940d45224138edc507f727a83d3d01f9823a3d
Jchan6611 <chunwa_c@yahoo.com>
2016-04-17 01:57:14 -0400

    Added files via upload

---
+++ estimates_summaries_controller.rb
@@ -0,0 +1,16 @@
+class EstimatesSummariesController < ApplicationController
+  def index 
+  end
+  
+  def search
+    if params[:state].upcase=="ALL"
+       	@estimates_summaries = Estimates_summary.all
+    else
+    	@estimates_summaries = Estimates_summary.where("STATE = ?", params[:state].upcase)
+    end
+      respond_to do |format|
+      format.html{}
+     end
+  end
+
+end
